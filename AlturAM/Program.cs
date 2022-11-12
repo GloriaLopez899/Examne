bbe2f588af884e1b4a4df1bc1f824d18631740de
Mercedes López <glorialopez4578@gmail.com>
2022-11-12 08:46:22 -0600

    : Gloria examen

---
+++ AlturAM/Program.cs
@@ -0,0 +1,12 @@
+﻿float altura;
+Console.WriteLine("Ingerese su altura en emtros");
+altura = float.Parse(Console.ReadLine());
+
+if (altura <= 1.5);
+    Console.WriteLine("Es de altura baja " + altura + "m");
+    Console.WriteLine();
+{
+    if (altura > 1.5) ;
+    Console.Write("Persona de altura alta " + altura + "m");
+}
+Console.ReadKey();
---
+++ TurnoSueldos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Sueldos
+{
+    class Sueldos
+    {
+        private float[] turnoM; //Declaramos un vector para el turno mañana
+        private float[] turnoT; //Declaramos un vector para el turno tarde
+        public void Cargar()
+        {
+            string linea;
+            turnoM = new float[6]; //Asignamos al vector una de longitud 4
+            turnoT = new float[3]; //Asignamos al vector una de longitud 4
+            Console.WriteLine("Sueldos de empleados del turno de la mañana.");
+            for (int f = 0; f < 6; f++)
+            {
+                Console.Write("Ingrese sueldo " + (f + 1) + ": ");
+                linea = Console.ReadLine();
+                turnoM[f] = float.Parse(linea); //Asignamos valores al vector turno mañana
+            }
+            Console.WriteLine("Sueldos de empleados del turno de la tarde.");
+            for (int f = 0; f < 3; f++)
+            {
+                Console.Write("Ingrese sueldo " + (f + 1) + ": ");
+                linea = Console.ReadLine();
+                turnoT[f] = float.Parse(linea); //Asignamos valores al vector turno tarde
+            }
+        }
+        public void CalcularGastos()
+        {
+            float man = 0;
+            float tar = 0;
+            for (int f = 0; f < 8; f++)
+            {
+                man = man + turnoM[f]; //Realizamos la suna de los valores del vector turno Mañana
+                tar = tar + turnoT[f]; //Realizamos la suna de los valores del vector turno Tarde
+            }
+            Console.WriteLine("Total de gastos del turno de la mañana:" + man);
+            Console.WriteLine("Total de gastos del turno de la tarde:" + tar);
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Sueldos pv = new Sueldos();
+            pv.Cargar();
+            pv.CalcularGastos();
+        }
+    }
+}
